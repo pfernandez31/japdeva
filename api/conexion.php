d02655a08602b75f76e00edc52353d91049fb27c
gsusalvarado <jalvarado@appsoftcr.com>
2018-08-30 17:27:29 -0600

    db conexion

--- api/conexion.php
+++ api/conexion.php
@@ -8,17 +8,13 @@
 
 	$usuario = "root";
 
-	$password = "";//"b43e7f69fa152ed976a2349d4a2d49b99ab275501195c047"; //"";
+	$password = "b43e7f69fa152ed976a2349d4a2d49b99ab275501195c047";
 
 	$bd = "form_antecedentes";
 
 	try{
-
 		$cnn = new PDO("mysql:host=$servidor;dbname=$bd;charset=utf8",$usuario,$password);
-		//Excepciones
-
 		$cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 	}
 
 	catch(PDOException $e){
