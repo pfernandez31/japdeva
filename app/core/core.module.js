faf490c030f9878016d63a48e150496b3935d82b
gsusalvarado <jalvarado@appsoftcr.com>
2018-11-18 07:55:22 -0600

    momentjs

--- app/core/core.module.js
+++ app/core/core.module.js
@@ -7,7 +7,8 @@
       'ngCookies',
       'ui.bootstrap',
       'oitozero.ngSweetAlert',
-      'ui.date'
+      'ui.date',
+      'angularMoment'
     ])
     .filter('startFrom', function() {
     	return function(input, start) {
@@ -15,4 +16,23 @@
         	return input.slice(start);
     	}
 	   })
+    .config(function(){
+      moment.locale('es', {
+          relativeTime : {
+            future: "hace %s",
+            past:   "hace %s",
+            s:  "segundos",
+            m:  "un minuto",
+            mm: "%d minutos",
+            h:  "una hora",
+            hh: "%d horas",
+            d:  "un día",
+            dd: "%d días",
+            M:  "un mes",
+            MM: "%d meses",
+            y:  "a año",
+            yy: "%d años"
+          }
+      });
+    })
 })();
